3643859a91676d38a4c442b8ec32b3ae5e3a6084
Deaath65 <deaath65@gmail.com>
2023-05-25 21:29:54 +0545

    views done

--- project_student_enquiry_system/app_courses/views.py
+++ project_student_enquiry_system/app_courses/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app_courses.forms import CourseCreateForm
+from app_courses.models import CourseModel
 
 # Create your views here.
+def course_index(request):
+    courses = CourseModel.objects.all()
+
+    context = {
+        "title": "SES | Courses", 
+        "body_title": "Here are the courses details",
+        "courses": courses
+    }
+    return render(request, 'courses/index.html', context)
+
+def course_create(request):
+    form = CourseCreateForm()
+    context = {"form": form}
+
+    if request.method == "POST":
+        name = request.POST.get("course_name")
+        code = request.POST.get("course_code")
+        cm = CourseModel(course_name=name, course_code=code)
+        cm.save()
+
+        # cm = CourseModel()
+        # cm.course_name = request.POST.get("course_name")
+        # cm.course_code = request.POST.get("course_code")
+        # cm.save()
+        return redirect("course-index")
+
+    return render(request, 'courses/create.html', context)
+
+def course_edit(request, id):
+    course = CourseModel.objects.get(id=id)
+    context = {"course": course}
+    return render(request, 'courses/edit.html', context)
+
+def course_update(request):
+    if request.method == "POST":
+        instance = CourseModel.objects.get(id=request.POST.get('id'))
+        data = CourseCreateForm(data=request.POST, instance=instance)
+        if data.is_valid():
+            data.save()
+            return redirect("course-index")
+        return redirect("course-index")
+    return redirect("course-index")
+
+def course_show(request, id):
+    course = CourseModel.objects.get(id=id)
+    context = {"course": course}
+    return render(request, 'courses/show.html', context)
+
+def course_delete(request, id):
+    course = CourseModel.objects.get(id=id)
+    course.delete()
+    return redirect("course-index")
--- project_student_enquiry_system/app_students/views.py
+++ project_student_enquiry_system/app_students/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app_students.models import StudentModel
+from app_students.forms import StudentCreateForm
 
 # Create your views here.
+def student_create(request):
+    form = StudentCreateForm()
+    context = {"form": form}
+
+    if request.method == "POST":
+        std = StudentCreateForm(request.POST)
+        if std.is_valid():
+            std.save()
+            return redirect("student-index")
+        return redirect("student-create")
+    return render(request, 'students/create.html', context)
+
+def student_index(request):
+    students = StudentModel.objects.all()
+    context = {
+        "students" : students, 
+        "title": "SES | Student List", 
+        "body_title": "Here are the students list"
+        }
+    return render(request, "students/index.html", context)
+
+def student_edit(request, id):
+    student = StudentModel.objects.get(id=id)
+    form = StudentCreateForm(instance=student)
+    context = {"form": form}
+    if request.method == "POST":
+        std = StudentCreateForm(data=request.POST, instance=student)
+        if std.is_valid():
+            std.save()
+            return redirect("student-index")
+        return redirect("student-edit", id)
+    return render(request, 'students/edit.html', context)
+
+def student_show(request, id):
+    student = StudentModel.objects.get(id=id)
+    context = {"student": student}
+    return render(request, 'students/show.html', context)
+
+def student_delete(request, id):
+    student = StudentModel.objects.get(id=id)
+    student.delete()
+    return redirect("student-index")
